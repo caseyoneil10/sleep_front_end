12a4f23fd0fca89ab3d9114ccfaf6c2a8109cf85
Casey O'Neil <caseyoneil@haleys-MacBook-Pro.local>
2022-06-14 07:20:34 -0700

    read functionality added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [sleepData, setSleepData] = useState([])
+
+  const getSleepData = () => {
+    axios
+    .get('https://damp-ocean-33580.herokuapp.com/api/sleepData')
+    .then(response => setSleepData(response.data),
+    (err) => console.error(err)
   )
+    .catch((error) => console.error(error))
+  }
+
+  useEffect(() => {
+    getSleepData()
+  }, [])
+
+
+  return (
+    <>
+        <h1>Sleep Tracker</h1>
+        {sleepData.map((sleep) => {
+          return(
+            <div>
+            <h3>Name:{sleep.name}</h3>
+            <h3>Name:{sleep.age}</h3>
+            <h3>Name:{sleep.date}</h3>
+            <h3>Name:{sleep.hoursSlept}</h3>
+            <h3>Name:{sleep.routine}</h3>
+            <h3>Name:{sleep.sleepQuality}</h3>
+            </div>
+
+
+
+
+          )
+        })}
+    </>
+
+
+  )
+
+
+
 }
 
 export default App;
